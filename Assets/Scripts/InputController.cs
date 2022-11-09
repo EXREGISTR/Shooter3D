aabefefefffa084ac7bc04c69e4b5bc6bab4df20
Рафаэль <115467831+EXREGISTR@users.noreply.github.com>
2022-11-09 18:16:30 +0300

    Добавьте файлы проекта.

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    #region AxisNames
+    private const string Vertical = "Vertical";
+    private const string Horizontal = "Horizontal";
+    #endregion
+
+    [SerializeField] private PlayerMovement Player;
+
+    private void Update()
+    {
+        Walk();
+
+        if (Input.GetKey(KeyCode.Space))
+            Jump();
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            Run();
+    }
+    private void Walk()
+    {
+        Player.Walk(Input.GetAxis(Vertical), Input.GetAxis(Horizontal));
+    }
+    private void Run()
+    {
+        Player.Run(Input.GetAxis(Vertical), Input.GetAxis(Horizontal));
+    }
+    private void Jump()
+    {
+        Player.Jump();
+    }
+}
